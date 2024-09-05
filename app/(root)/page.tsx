c49929897fb612f094a6a97d2649c0cee5b139fe
Brayan Markov <brayan_markov@yahoo.com>
2024-09-05 20:18:21 +0300

    Add bank card templates

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
 
-    const loggedIn = { firstName: 'Brayan' };
+    const loggedIn = { firstName: 'Brayan', lastName: 'JSM', email: 'brayan_m@gmail.com' };
 
   return (
     <section className='home'>
@@ -23,7 +24,13 @@ const Home = () => {
                 totalCurrentBalance={1250.35}
                 />
             </header>
+            RECENT TRANSACTIONS
         </div>
+        <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[]}
+        />
     </section>
   )
 }
